47b0992c3ff0e0c37972f4fd5e2d6a6b8e7c5084
Sunho Kim <ksunhokim123@naver.com>
2019-04-09 12:23:58 +0900

    Opps

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/docs'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname + '/docs', 'index.html'));
 });
 
 app.listen(port);
